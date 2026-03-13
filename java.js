1b7d2768f372fb3508e94d40e5a3f2f2966b6a27
hamnajamil18-eng <hamnajamil18@gmail.com>
2026-03-14 00:15:02 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,104 @@
+var num1='10'
+var num2='30'
+var sum=Number(num1)+Number(num2)
+console.log(sum)
+var numb1='10'
+var numb2='30'
+// qustion:2
+var sub=Number(numb1)-Number(numb2)
+console.log(sub)
+
+var numb1='10'
+var numb2='30'
+var mul=Number(numb1)*Number(numb2)
+console.log(mul)
+
+var numb1='10'
+var numb2='30'
+var div= Number(numb1)/(numb2)
+console.log(div)
+
+// question:3
+ 
+var num=
+console.log("value after declaration is:" + num)
+ var num='5'
+ console.log("intial value:" + num)
+
+ num++;
+ console.log("value after increment is:" + num)
+ num=num+7;
+ console.log("value after addition is:" + num)
+ num--;
+ console.log("value after decrement is"+num)
+ 
+var remiander=num%3;
+console.log("The remainder is:" + remiander)
+
+// question:4
+var ticketprice=600
+var cost=ticketprice*5
+console.log("The total cost to buy 5 tickets is:" + cost)
+
+// question:5
+var table=4
+var tablevalue1=table*1
+var tablevalue2=table*2
+var tablevalue3=table*3
+var tablevalue4=table*4
+var tablevalue5=table*5
+var tablevalue6=table*6
+var tablevalue7=table*7
+var tablevalue8=table*8
+var tablevalue9=table*9
+var tablevalue10=table*10
+console.log("table of 4")
+console.log("4x1=" + tablevalue1)
+console.log("4x2=" + tablevalue2)
+console.log("4x3=" + tablevalue3)
+console.log("4x4=" + tablevalue4)
+console.log("4x5=" + tablevalue5)
+console.log("4x6=" + tablevalue6)
+console.log("4x7=" + tablevalue7)
+console.log("4x8=" + tablevalue8)
+console.log("4x9=" + tablevalue9)
+console.log("4x10=" + tablevalue10)
+
+// qustion:6
+var celcius=25
+var farhenheit=(celcius*9/5)+32;
+console.log (celcius+"c is"+farhenheit+"f")
+var farhenheit=70
+var celcius=(farhenheit-32)*5/9
+console.log(farhenheit+"f is"+celcius+"c")
+
+// question:7
+var item1=650
+var quantity1=3
+var item2=100
+var quantity2=7
+var shipping=100
+var totalcost=(item1*quantity1)+(item2*quantity2)+shipping
+console.log("price of item 1 is"+item1)
+console.log("quantity of item 1 is"+quantity1)
+console.log("price of item 2 is"+item2)
+console.log("quantity of item 2 is" +quantity2)
+console.log("shipping charges"+shipping)
+console.log("the total cost of your order is"+totalcost)
+
+
+// question:8
+var totalmarks=980
+var marksobtained=804
+var percentage=(marksobtained/totalmarks)*100
+console.log("total marks: " + totalmarks)
+console.log("marks obtained: " + marksobtained)
+console.log("percentage: " + percentage + "%")
+
+// question:9
+var usdollars=10
+var riyals=25
+var usdrate=104.80
+var riyalrate=28
+
+
---
+++ script.js
@@ -0,0 +1,87 @@
+alert("error?please enter a valid password");
+alert("welcome to js land\n happy coding")
+ alert("welcome to js land.....")
+alert("happy coding!\n presvent this page from craeting additional dialogs..")
+ alert("hello...ican run js through my web browser's console")
+// // chapter 2
+ var username='john doe';
+ username='jamil';
+ console.log(username);
+ var age='15 ';
+ alert(age + 'year old' )
+ var massege='certified mobile application development';
+ alert(massege)
+ var username="pizza\npizz\npiz\npi\np"
+alert(username)
+var email='@example.com'
+alert('my email address is example'+email)
+var book="a smarter way to learn javascript"
+alert('i am trying to learn from the book' + book)
+
+
+// // chapter 3
+ var age='15';
+ alert('i am' + age + 'year old' )
+ var visit='14'
+ alert('you have visited this site' + visit + 'times')
+ var birthyear='1990'
+  alert(birthyear)
+ var visitername='john doe'
+ var producttittle='t-shirt'
+ var quantity='5'
+ console.log(visitername + '' + producttittle + '' + quantity + '' + '(s) on xyz clothing store')
+
+//  cahpter 4
+var username='aiman'
+var age='18'
+var country='pakistan'
+console.log(username)
+console.log(age)
+console.log(country)
+
+var username='hamna'
+var age='14'
+var country='pakistan'
+var city='karachi'
+var qualification='intermediate'
+console.log(username)
+console.log(age)
+console.log(country)
+console.log(city)
+console.log(qualification)
+
+var heading='rules for naming js varialbes'
+console.log(heading)
+
+
+
+// chapter 5
+
+//   var username='saad'
+
+//  console.log(username)
+//  var maths='95'
+//  var physics='88'
+// var chemistry='92'
+// var totalmarks='300'
+// var obtained=Number(maths)+Number(physics)+Number(chemistry)
+// var percentange=(obtained/totalmarks)*100
+// console.log(percentange)
+// var username='sadd'
+// console.log(username)
+// var maths='90'
+// var physics='60'
+// var chemistry='50'
+// var totalmarks='300'
+// var obtained= Number (maths)+ Number (physics) + Number(chemistry)
+// var percentage= (obtained/totalmarks)*100
+// console.log(percentage)
+// var username=Number(prompt('Enter your name'))
+// var value1=Number(prompt('Enter your first value'))
+// var value2=Number(prompt('Enter your second value'))
+// var value3=Number(prompt('enter your third value'))
+// console.log(value1 +value2+value3)
+// var totalmarks='300'
+// var obtained=Number(value1)+Number(value2)+Number(value3)
+// var percentage=(obtained/totalmarks)*100
+// console.log(percentage)
